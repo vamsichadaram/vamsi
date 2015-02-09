d79007f698371c0d9f959e3a2dc30422a13884dc
Trainee <Trainee@acaluk.acalpreprod1>
2015-02-09 19:07:53 +0530

    folders

--- demo/src/com/srusti/service/FolderService.java
+++ demo/src/com/srusti/service/FolderService.java
@@ -2,7 +2,10 @@ package com.srusti.service;
 
 import java.io.File;
 
+import javax.servlet.ServletContext;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.srusti.controller.constants.SampleCons;
@@ -11,17 +14,20 @@ import com.srusti.controller.constants.SampleCons;
 public class FolderService 
 {
 	private static Logger LOG= Logger.getLogger(FolderService.class);
+	@Autowired
+	private ServletContext context;
 	public void createFolders(String name,int id)
 	{
-		String temp1=SampleCons.MEDIA_PATH+File.separator+name+"_"+id;
-		File file= new File(temp1);
+		String temp=SampleCons.MEDIA_PATH+File.separator+File.separator;
+		String temp1=SampleCons.MEDIA_PATH+name+"_"+id;
+		File file= new File(context.getContextPath()+File.separator+temp);
 		if(!file.exists())
 		{
 			LOG.info("creating media folders to the "+name+" customer");
-			file.mkdir();
-			File file1= new File(File.separator+temp1+"wedding");
+			LOG.info(context.getContextPath()+File.separator+temp1+" wedding");
+			File file1= new File(context.getRealPath(File.separator)+temp1);
 			file1.mkdir();
-			File file2= new File(File.separator+temp1+"birthday");
+			File file2= new File(context.getRealPath(File.separator)+temp1);
 			file2.mkdir();
 		}
 		else
